12178ec29c5d996f198458c7ff9e1524f2a7de29
gureumwoon <vfrvfr3221@gmail.com>
2024-02-14 22:27:20 +0900

    프로젝트에 useQuery적용

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,12 +1,23 @@
 import React, { useState } from 'react';
-import useProducts from '../hooks/use-products';
+import { useQuery } from '@tanstack/react-query';
 
 export default function Products() {
   const [checked, setChecked] = useState(false);
-  const [loading, error, products] = useProducts({ salesOnly: checked });
+  const {
+    isLoading,
+    error,
+    data: products,
+  } = useQuery({
+    queryKey: ['products'],
+    queryFn: async () => {
+      const response = await fetch(`data/products.json`)
+      return response.json()
+    },
+  })
+  // const [loading, error, products] = useProducts({ salesOnly: checked });
   const handleChange = () => setChecked((prev) => !prev);
 
-  if (loading) return <p>Loading...</p>;
+  if (isLoading) return <p>Loading...</p>;
 
   if (error) return <p>{error}</p>;
 
